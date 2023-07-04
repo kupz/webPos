6ec851b7b36f24ea6a0c1b8e81a90d06930fe06a
Kupz <benjohnvillabesa@gmail.com>
2023-07-04 15:42:03 +0800

    something added

--- src/pages/webpos/Webpos.js
+++ src/pages/webpos/Webpos.js
@@ -2,17 +2,32 @@ import MenuIcon from "@mui/icons-material/Menu";
 import QrCodeScannerIcon from "@mui/icons-material/QrCodeScanner";
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useState } from "react";
 
 export default function Webpos() {
+  const [menu, setMenu] = useState(false);
   return (
     <div className="webpos">
       <div className="webpos-content">
         <div className="webpos-products">
           <div className="product-nav">
-            <span>
-              <MenuIcon />
+            <span className="position-relative">
+              <span onClick={() => setMenu(!menu)}>
+                <MenuIcon />
+              </span>
+              <div
+                className="expandable-menu"
+                style={{
+                  display: menu ? "flex" : "none",
+                  zIndex: "-1",
+                }}
+              >
+                <span>Dashboard</span>
+                <span>Add Product</span>
+                <span></span>
+              </div>
             </span>
-            <span>
+            <span className="searchBar">
               <input type="text" />
               <SearchIcon />
             </span>
